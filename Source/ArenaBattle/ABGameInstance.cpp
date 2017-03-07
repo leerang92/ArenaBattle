33d760e49a79d22a1e6295d10864f74b791e2670
LeeRang <asw77id@gmail.com>
2017-03-07 13:33:53 +0900

    Add files via upload

---
+++ Source/ArenaBattle/ABGameInstance.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ArenaBattle.h"
+#include "ABGameInstance.h"
+
+void UABGameInstance::Init()
+{
+	Super::Init();
+
+	UE_LOG(LogClass, Error, TEXT("ArenaBattle Game Instance"));
+}
+
+
