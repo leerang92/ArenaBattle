41269c6e7474374b238f141ded24815983badab5
LeeRang <asw77id@gmail.com>
2017-03-21 15:46:53 +0900

    3주차
    모듈, 로그 매크로, UClass, 리플렉션

--- Source/WebService/Private/WebConnect.cpp
+++ Source/WebService/Private/WebConnect.cpp
@@ -4,5 +4,16 @@
 #include "WebConnect.h"
 
 
+UWebConnect::UWebConnect()
+{
+	UE_LOG(LogClass, Warning, TEXT("WebConnect Contructor"));
 
+	Host = TEXT("localhost:8000");
+	URI = TEXT("/");
+}
+
+void UWebConnect::RequestToken()
+{
+	UE_LOG(LogClass, Warning, TEXT("Request Token Call!"));
+}
 
